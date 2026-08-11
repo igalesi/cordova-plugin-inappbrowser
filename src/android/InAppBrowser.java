12bdcbbb08ac39fda24ebc501c16742ae4c7dd6f
Ivan Galesi <Ivan.Galesi@senior.com.br>
2026-08-11 16:51:28 -0300

    Fix custom scheme regex: use + quantifier to avoid matching bare ://

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1381,7 +1381,7 @@ public class InAppBrowser extends CordovaPlugin {
                 }
             }
             // Test for whitelisted custom scheme names like mycoolapp:// or twitteroauthresponse:// (Twitter Oauth Response)
-            else if (!url.startsWith("http:") && !url.startsWith("https:") && url.matches("^[A-Za-z0-9+.-]*://.*?$")) {
+            else if (!url.startsWith("http:") && !url.startsWith("https:") && url.matches("^[A-Za-z0-9+.-]+://.*$")) {
                 if (allowedSchemes == null) {
                     String allowed = preferences.getString("AllowedSchemes", null);
                     if(allowed != null) {
